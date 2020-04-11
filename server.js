14981fe33f60833ff76220ac58c965867d038e4f
ronen <ronen567@gmail.com>
2020-04-11 23:29:26 +0300

    fix connectionString

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const profile = require('./controllers/profile')
 // taken from knex site
 const db = knex({  client: 'pg',
   connection: {
-  	host: process.env.DATABASE_URL,
+  	connectionString: process.env.DATABASE_URL,
   	ssl: true,
   }
 });
